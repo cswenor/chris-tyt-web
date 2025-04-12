86b143fd709906de5f2d0be1cd740aa1b7f013d2
Beeb <opeyemihabeebullah279@gmail.com>
2025-04-12 01:08:40 +0100

    Quick check

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
+import { webpackFallback } from '@txnlab/use-wallet-react';
+
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        ...webpackFallback,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
